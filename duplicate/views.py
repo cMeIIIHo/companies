550be85a5e842367f117f9e5d32058abdd549886
cMeIIIHo <cMeIIIHo7@gmail.com>
2018-01-27 17:57:00 +0300

    pep

--- duplicate/views.py
+++ duplicate/views.py
@@ -11,13 +11,13 @@ url = f"https://{domain}/rest/crm.company.list.json"
 @xframe_options_exempt
 @csrf_exempt
 def show_duplicates(request):
-    # access_token = '92qtu0iet35fxn79dn444czk3i7e79kh'
-    # params = {'auth': access_token}
-    # r = requests.get(url=url, params=params)
-    # companies = r.json()['result']
-    # company_names = {}
-    # for company in companies:
-    #     company_names[company['TITLE']] = company_names.get(company['TITLE'], [])
-    #     company_names[company['TITLE']].append(company['ID'])
-    # duplicates = {name: identifiers for name, identifiers in company_names.items() if len(identifiers) > 1}
-    return JsonResponse({'a': 1})
+    access_token = request.POST['access_token']
+    params = {'auth': access_token}
+    r = requests.get(url=url, params=params)
+    companies = r.json()['result']
+    company_names = {}
+    for company in companies:
+        company_names[company['TITLE']] = company_names.get(company['TITLE'], [])
+        company_names[company['TITLE']].append(company['ID'])
+    duplicates = {name: identifiers for name, identifiers in company_names.items() if len(identifiers) > 1}
+    return JsonResponse(duplicates)
