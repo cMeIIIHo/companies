ccdb3fa5ee76b21eaa72f2f860611edbf0f11e4f
cMeIIIHo <cMeIIIHo7@gmail.com>
2018-01-29 08:37:36 +0300

    delete unused code

--- duplicate/static/duplicate/js/duplicate.js
+++ duplicate/static/duplicate/js/duplicate.js
@@ -2,14 +2,14 @@ $(document).ready(function () {
 
   BX24.init(function(){
 
-  Vue.component('company_id', {
-    props: ['id'],
-    template: `<li>ID: {{ id }}</li>`
-  });
+    Vue.component('company_id', {
+      props: ['id'],
+      template: `<li>ID: {{ id }}</li>`
+    });
 
-  Vue.component('each_name_block', {
-    props: ['name'],
-    template: `
+    Vue.component('each_name_block', {
+      props: ['name'],
+      template: `
         <li>Для компании с названием "{{ name.name }}" зарегистрированы дубликаты с идентификаторами:
           <ul>
             <company_id
@@ -19,47 +19,32 @@ $(document).ready(function () {
             </company_id>
           </ul>
         </li>
-    `
-  });
+      `
+    });
 
-  var vm = new Vue({
-    el: '#vm',
-    data: {
-      duplicates: []
-    },
-    methods: {
-      show_duplicates: function() {
-        this.duplicates = []
-        var self = this;
-        $.ajax({
-          url: 'http://127.0.0.1:8000/api/get_duplicates',
-          type: 'POST',
-          data: BX24.getAuth(),
-          dataType: 'json',
-          success: function(duplicates){
-            for(var index in duplicates) {
-              self.duplicates.push(duplicates[index])
-            };
-            console.log(self.duplicates)
-          }
-        })
+    var vm = new Vue({
+      el: '#vm',
+      data: {
+        duplicates: []
+      },
+      methods: {
+        show_duplicates: function() {
+          this.duplicates = []
+          var self = this;
+          $.ajax({
+            url: 'http://127.0.0.1:8000/api/get_duplicates',
+            type: 'POST',
+            data: BX24.getAuth(),
+            dataType: 'json',
+            success: function(duplicates){
+              for(var index in duplicates) {
+                self.duplicates.push(duplicates[index])
+              }
+            }
+          })
+        }
       }
-    }
-  });
-
-
+    });
   });
 });
 
-function get_duplicates() {
-  var self = this;
-  $.ajax({
-    url: 'http://127.0.0.1:8000/api/get_duplicates',
-    type: 'POST',
-    data: BX24.getAuth(),
-    dataType: 'json',
-    success: function(duplicates){
-      self.duplicates.push(duplicates);
-    }
-  });
-};
